a0705601d5e778a368377104702a219ac5f5afbb
amirulasri <amirulasrix@gmail.com>
2022-03-05 00:57:00 +0800

    Testing SQL

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,10 @@
 package main;
 
 import java.util.ArrayList;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
 	static ArrayList<Customerclass> customers = new ArrayList<Customerclass>();
@@ -12,6 +16,7 @@ public class Main {
 	static private double discount = 0.10;
 	static private String appname = "Bakery Shop";
 	static private String contributor = "Amirul Asri, Harris Irfan, Sholihin Ilias, Aliff Redzuan, Mifzal Dini";
+	static String url = "jdbc:sqlite:db/bakeryorderdata.db";
 	
 	static public String getappname() {
 		return appname;
@@ -40,11 +45,56 @@ public class Main {
 	public static ArrayList<Paymentclass> getpayment(){
 		return listpayment;
 	}
+	
+	public static void createNewTable() {
+        // SQL statement for creating a new table
+        String sql = "CREATE TABLE IF NOT EXISTS customer (\n"
+                + "	id integer PRIMARY KEY,\n"
+                + "	name text NOT NULL,\n"
+                + "	phoneno text NOT NULL,\n"
+                + "	address text NOT NULL,\n"
+                + "	gender text NOT NULL,\n"
+                + "	regularcustomer numeric NOT NULL\n"
+                + ");";
+        
+        try (Connection conn = DriverManager.getConnection(url);
+                Statement stmt = conn.createStatement()) {
+            // create a new table
+            stmt.execute(sql);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+	
+	public static void connect() {
+        Connection conn = null;
+        try {
+            // db parameters
+            // create a connection to the database
+            conn = DriverManager.getConnection(url);
+            
+            System.out.println("Connection to SQLite has been established.");
+            
+            
+            
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
 
 	public static void main(String[] args) {
+		createNewTable();
+		/*
 		Welcomeframe welcomeframe;
 		Cashierframe cashier;
-
 		try {
 			welcomeframe = new Welcomeframe();
 			cashier = new Cashierframe();
@@ -67,5 +117,6 @@ public class Main {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		*/
 	}
 }
