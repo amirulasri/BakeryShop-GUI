98a063a9e415e22d58bef906769afb713f68d519
amirulasri <amirulasrix@gmail.com>
2022-03-10 21:33:54 +0800

    Fix bug Order Frame

--- src/main/Payment.java
+++ src/main/Payment.java
@@ -220,6 +220,7 @@ public class Payment extends JFrame {
 			            NewOrder.setpaymentframenull();
 			            NewOrder.listpricecust = 0;
 						NewOrder.finalprice = 0;
+						NewOrder.regularcustomer = false;
 					} catch (SQLException e1) {
 						System.out.println("SQL ERROR: " + e1.getMessage());
 					} catch (Exception e1) {
